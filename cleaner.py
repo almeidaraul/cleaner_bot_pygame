23bbffcf87f3ed743f930751c2474f41ef8adb02
Raul Almeida <haltsimog@gmail.com>
2020-11-27 16:40:54 -0300

    perfect; only things is, this is not pygame yet

--- cleaner.py
+++ cleaner.py
@@ -6,47 +6,60 @@ real_map = [[0, 1, 0, 0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 1, 0]]
 
 def sensor(s):
     """
-    s: array
+    s: array of coordinates (row, column)
     return: n, given that nth element of s is first to be non-zero
         obs: n is -1 if s is only composed of zeros
     """
     for i in range(len(s)):
-        if s[i]:
+        if real_map[s[i][0]][s[i][1]]:
             return i
     return -1
 
 class Robot():
     def __init__(self, map_size):
-        self.pos = [0, 0] //row, column
+        self.pos = [0, 0] #row, column
         self.map_size = map_size
         self.map = [[0]*map_size for m in range(map_size)]
-        self.map[self.pos[0]][self.pos[1]] = 1
-        print(self.map)
+
+        # positioning our bot
+        self.map[self.pos[0]][self.pos[1]] = 2
+        sensor_results = self.sense()
+        for t in sensor_results:
+            v = sensor_results[t]
+            if v:
+                self.map[v[0]][v[1]] = 1
+
+    def print_map(self):
+        for row in self.map:
+            print(row)
 
     def sense(self):
-        top = [self.map[x][self.pos[1]] for x in range(self.pos[0])]
+        top = [[x, self.pos[1]] for x in range(self.pos[0])]
         sensor_value = sensor(top)
-        top = [sensor_value, self.pos[1]] if sensor_value != -1 else None
+        top = top[sensor_value] if sensor_value != -1 else None
 
-        bottom = [self.map[x][self.pos[1]] for x in range(self.pos[0]+1, self.map_size)]
+        bottom = [[x, self.pos[1]] for x in range(self.pos[0]+1, self.map_size)]
         sensor_value = sensor(bottom)
-        bottom = [sensor_value+self.pos[0]+1, self.pos[1]] if sensor_value != -1 else None
+        bottom = bottom[sensor_value] if sensor_value != -1 else None
 
-        left = [self.map[self.pos[0]][x] for x in range(self.pos[1])]
+        left = [[self.pos[0], x] for x in range(self.pos[1])]
         sensor_value = sensor(left)
-        left = [self.pos[0], sensor_value] if sensor_value != -1 else None
+        left = left[sensor_value] if sensor_value != -1 else None
 
-        right = [self.map[self.pos[0]][x] for x in range(self.pos[1]+1, self.map_size)]
+
+        right = [[self.pos[0], x] for x in range(self.pos[1]+1, self.map_size)]
         sensor_value = sensor(right)
-        right = [self.pos[0], snesor_value+self.pos[1]+1] if sensor_value != -1 else None
-        return {'top': top, 'right': right, 'bottom': bottom, 'left': left]
+        right = right[sensor_value] if sensor_value != -1 else None
+        return {'top': top, 'right': right, 'bottom': bottom, 'left': left}
 
     def move(self, new_pos):
-        if self.map[new_pos]:
+        if new_pos[0] < 0 or new_pos[0] >= self.map_size or new_pos[1] < 0 or new_pos[1] >= self.map_size:
+            return 1
+        if self.map[new_pos[0]][new_pos[1]]:
             return 1 # can't move there
-        elif self.pos != new_pos:
+        if self.pos[0] != new_pos[0] or self.pos[1] != new_pos[1]:
             self.map[self.pos[0]][self.pos[1]] = 0
-            self.map[new_pos[0]][new_pos[1]] = 1
+            self.map[new_pos[0]][new_pos[1]] = 2
             self.pos = new_pos
             sensor_results = self.sense()
             for t in sensor_results:
@@ -54,3 +67,25 @@ class Robot():
                 if v:
                     self.map[v[0]][v[1]] = 1
         return 0
+
+print("Real map:")
+for r in real_map:
+    print(r)
+
+r = Robot(4)
+print("Robot vision:")
+r.print_map()
+entra = input()
+while entra in ['w', 'a', 's', 'd']:
+    new_pos = [r.pos[0], r.pos[1]]
+    if entra == 'w':
+        new_pos[0] -= 1
+    elif entra == 's':
+        new_pos[0] += 1
+    elif entra == 'a':
+        new_pos[1] -= 1
+    elif entra == 'd':
+        new_pos[1] += 1
+    r.move(new_pos)
+    r.print_map()
+    entra = input()
